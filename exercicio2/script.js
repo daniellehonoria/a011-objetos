de50872b3212de474e580a56a85deb2ea3d4f388
daniellehonoria <daniellehonoria@gmail.com>
2022-09-05 12:52:01 -0300

    Resolução dos exercícios de objetos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,26 @@
 const filme = {
-	nome: "Auto da Compadecida", 
-	ano: 2000, 
+	nome: "Auto da Compadecida",
+	ano: 2000,
 	elenco: [
-		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga",
 		"Virginia Cavendish"
-		], 
+	],
 	transmissoesHoje: [
-		{canal: "Telecine", horario: "21h"}, 
-		{canal: "Canal Brasil", horario: "19h"}, 
-		{canal: "Globo", horario: "14h"}
-		]
+		{ canal: "Telecine", horario: "21h" },
+		{ canal: "Canal Brasil", horario: "19h" },
+		{ canal: "Globo", horario: "14h" }
+	]
 };
 
 // respostas aqui ⬇️
+
+//a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
+console.log("Primeiro ator: ", filme.elenco[0])
+
+//b) Qual o código para imprimir o nome do **último** ator/atriz?
+console.log("Último ator: ", filme.elenco[3])
+
+//c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+console.log("As transmissões de hoje são", filme.transmissoesHoje)
+//d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?
+console.log("O horário de transmissão do filme no Canal Brasil é:", filme.transmissoesHoje[1].horario)
